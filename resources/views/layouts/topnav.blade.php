5c72bfbb82ad045031c71ac9ec9d9b92aceeeabb
Bach <nguyen.van.bach@framgia.com>
2019-01-30 08:54:14 +0700

    change top nav client

--- resources/views/layouts/topnav.blade.php
+++ resources/views/layouts/topnav.blade.php
@@ -9,8 +9,13 @@
                     <ul>
                         <li><a href="help.html">Help</a></li>
                         <li><a href="pricing-plan.html">Pricing</a></li>
-                        <li><a href="#" data-toggle="modal" data-target="#register-model">Register</a></li>
-                        <li><a href="#" data-toggle="modal" data-target="#login-model">Login</a></li>
+                        @auth
+                            <li><a href="#">{{ Auth::user()->name }}</a></li>
+                            <li><a href="{{ route('logout') }}">{{ __('admin.logout') }}</a></li>
+                        @endauth
+                        @guest
+                            <li><a href="{{ route('login') }}">{{ __('admin.login') }} | {{ __('register') }}</a></li>
+                        @endguest
                         <li class="search-btn search-icon text-center">
                             <a href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
                         </li>
@@ -26,49 +31,5 @@
             </div>
         </div>
         <!-- end search -->
-        <!-- login popup -->
-        <div class="modal fade login-model" id="login-model" role="dialog">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h5 class="modal-title text-center">Login</h5>
-                    </div>
-                    <div class="modal-body login-model-body text-center">
-                        <form id="login-form" action="#">
-                            <div class="form-group">
-                                <input type="email" class="form-control" id="login_email" placeholder="Enter Your Email"/>
-                                <input type="password" class="form-control" id="login_password" placeholder="Enter Your Password"/>
-                            </div>
-                            <button type="submit" class="btn btn-default">Login</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- end login popup -->
-        <!-- register popup -->
-        <div class="modal fade register-model" id="register-model" role="dialog">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h5 class="modal-title text-center">Register</h5>
-                    </div>
-                    <div class="modal-body request-model-body text-center">
-                        <form id="register-form" action="#">
-                            <div class="form-group">
-                                <input type="text" class="form-control" id="reg_name" placeholder="Enter Your Name"/>
-                                <input type="email" class="form-control" id="reg_email" placeholder="Enter Your Email"/>
-                                <input type="password" class="form-control" id="reg_password" placeholder="Enter Your Password"/>
-                                <input type="password" class="form-control" id="reg_confirm-password" placeholder="Confirm Your Password"/>
-                            </div>
-                            <button type="submit" class="btn btn-default">Register</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- end register popup -->
     </div>
 </section>
